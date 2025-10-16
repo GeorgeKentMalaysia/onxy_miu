4e2c0947dbcfa8916ae494a22ac973f6b51e1b97
Ricky Siah <ricky.siah@georgekent.net>
2025-10-16 08:32:36 +0800

    Created bootloader skeleton

---
+++ App/bootloader.h
@@ -0,0 +1,29 @@
+/**
+  ******************************************************************************
+  * @file           : bootloader.h
+  * @brief          : Header for bootloader
+  ******************************************************************************
+  * @attention
+  * @PIC: muhammad.ahmad@georgekent.net
+  * @Created: 1 Mar 2022
+  * @Copyright: George Kent (Malaysia) Berhad.
+  *
+  ******************************************************************************
+**/
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef BOOTLOADER_H_
+#define BOOTLOADER_H_
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Exported types ------------------------------------------------------------*/
+
+/* Exported constants --------------------------------------------------------*/
+
+/* Exported macros -----------------------------------------------------------*/
+
+/* Exported functions --------------------------------------------------------*/
+int BTLD_Main(void);
+
+#endif /* BOOTLOADER_H_ */
